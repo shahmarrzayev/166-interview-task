542c58cf374f8e0e82dca5f504ef0137c7cc1f76
shahmarrzayev <shahmar.rza@gmail.com>
2024-09-08 22:42:14 +0400

    commit for netlify

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ setupAndCheckEnvironment();
 configureLogger();
 
 const app = express();
-
+app.use('/', (req, res) => res.send('Welcome to the API!'));
 configureCors(app);
 setupRoutes(app);
 setupDbConnection()
